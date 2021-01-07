af1692f78ab7ca9c20752bdc6b8f140c5e833f7c
shivani <shivani.bisaria11@gmail.com>
2021-01-06 19:49:01 -0800

    My first commit

---
+++ src/React/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/* Components ---------------------------*/
+// import Test from './Test.jsx';
+
+const Main = () => {
+
+    return (
+        <div className='Main'>
+            <h1>Shivani's final</h1>
+            
+        </div>
+    );
+}
+
+export default Main;
