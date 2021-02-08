8f751304d2fec29d610839a30a9e80f13600a955
shivani <shivani.bisaria11@gmail.com>
2021-02-08 15:25:27 -0800

    Updating homework for S9

--- src/React/Container.jsx
+++ src/React/Container.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
+/* Scripts ---------------------------*/
+import { mq } from '../common/media_queries.js';
+
+/* Components ---------------------------*/
 import Header from './Header.jsx';
 import Nav from './Nav.jsx'
 import Main from './Main.jsx';
@@ -10,14 +15,28 @@ const Container = () => {
 
     return (
         <BrowserRouter>
-            <div className='Container'>
+            <ContainerStyled className='Container'>
                 <Header />
                 <Nav />
                 <Main />
                 <Footer />
-            </div>
+            </ContainerStyled>
         </BrowserRouter>
     );
 }
 
 export default Container;
+
+const ContainerStyled = styled.div`
+    background-color: lavenderblush;
+
+    @media ${mq.tablet} {
+        background-color: yellow;
+
+    @media ${mq.desktop} {
+        background-color: blueviolet;
+    }
+    @media ${mq.ultrawide} {
+        background-color: peachpuff;
+    }
+`;
