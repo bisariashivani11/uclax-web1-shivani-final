d52ddffca3a554ca177765ae7b5f49e687277d74
shivani <shivani.bisaria11@gmail.com>
2021-02-23 17:26:58 -0800

    Updating Services: Lightboxes

--- src/React/Pages/Services/FilterNav/FilterNav.jsx
+++ src/React/Pages/Services/FilterNav/FilterNav.jsx
@@ -10,8 +10,15 @@ const FilterNav = ({ services, catChosen, catChosenUpdate }) => {
 
     return (
         <FilterNavStyled className='FilterNav'>
-            FilterNav 
-            <FilterNavButton />
+            {
+                services.categories.map((category, idx) => {
+                    return <FilterNavButton
+                        key={ idx }
+                        category={ category }
+                        catChosen={ catChosen }
+                        catChosenUpdate={ catChosenUpdate } />
+                })
+            }
         </FilterNavStyled>
     );
 }
@@ -20,4 +27,7 @@ export default FilterNav;
 
 const FilterNavStyled = styled.div`
     
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
 `;
